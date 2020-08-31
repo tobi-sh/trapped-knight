5b2637908ef260f3d3794f1c5cf21019767e65f7
Tobias Schulze-Heinrichs 👾 <tobias.schulze@xing.com>
2020-08-31 14:47:15 +0200

    able to move knight 1000 times

--- board.js
+++ board.js
@@ -1,84 +1,111 @@
-function calcRingSize(r) {
-    a = 0
-    for(i=0;i<r;i++) {
-      a += Math.pow(2*i+1,2)
-      console.log(i, ": ", 2*i+1, "(" , Math.pow(2*i+1,2), ") => ", a)
+function Position(x,y) {
+    this.x = x
+    this.y = y
+    this.move = function(dx, dy) {
+        return new Position(x + dx, y + dy)
     }
-    return a
 }
 
-function createBoard() {
-    rings = 12
-    boardSize = 2*rings - 1 
-    board = []
-    for (i=0;i<boardSize;i++) {
-        board.push(Array(boardSize).fill(0))
+function Board(size) {
+    this.boardSize = 2*size - 1 
+    this.cellSize = 50
+    this.tiles = []
+    for (i=0;i<this.boardSize;i++) {
+        this.tiles.push(Array(this.boardSize).fill(0))
     }
-    x = Math.ceil(boardSize/2)-1
-    y = Math.ceil(boardSize/2)-1
+    x = Math.ceil(this.boardSize/2)-1
+    y = Math.ceil(this.boardSize/2)-1
+    this.center = new Position(x,y)
     v  = 1
     // init
-    board[x--][y] = v
-    for (r = 1 ; r<rings;r++) {
+    this.tiles[x--][y] = v
+    for (r = 1 ; r<size;r++) {
         // up
         upstart = ++v
-        board[++x][--y] = upstart
+        this.tiles[++x][--y] = upstart
         // right
-        while(board[x][y+1] != 0){ 
-            board[++x][y] = ++v
+        while(this.tiles[x][y+1] != 0){ 
+            this.tiles[++x][y] = ++v
         }
         // down
-        while(board[x-1][y] != 0){ 
-            board[x][++y] = ++v
+        while(this.tiles[x-1][y] != 0){ 
+            this.tiles[x][++y] = ++v
         }
         //left
-        while(board[x][y-1] != 0){ 
-            board[--x][y] = ++v
+        while(this.tiles[x][y-1] != 0){ 
+            this.tiles[--x][y] = ++v
         }
         //up
-        while(board[x+1][y] != 0 && board[x+1][y] != upstart ){ 
-            board[x][--y] = ++v
+        while(this.tiles[x+1][y] != 0 && this.tiles[x+1][y] != upstart ){ 
+            this.tiles[x][--y] = ++v
         }
         // right
-        while(board[x+1][y] != upstart){ 
-            board[++x][y] = ++v
+        while(this.tiles[x+1][y] != upstart){ 
+            this.tiles[++x][y] = ++v
         }
-    }
-    return board
-}
-
-
-function debugPrintBoard(board) {
-    let a = new Intl.NumberFormat('en-IN', { minimumIntegerDigits: 3 })
-    xs = board.length
-    ys = board[0].length
-    for (y = 0; y < ys; y ++ ) {
-        s = ""
-        for (x = 0; x < xs ; x ++ ) {
-            s += a.format(board[x][y]) + ' '
-        }
-        console.log(s)
     }
 }
 
-function drawBoard(board) {
-    xs = board.length
-    ys = board[0].length
+let theBoard = new Board(20)
+
+theBoard.draw = function() {
+    xs = this.tiles.length
+    ys = this.tiles[0].length
     formatter = new Intl.NumberFormat('en-IN', { minimumIntegerDigits: 3 })
     filling = "yellow"
-    //    <rect width="50" height="50" x="500" y="500" style="fill:none;stroke-width:1;stroke:rgb(0,0,0)" id=r1 />
     for (y = 0; y < ys; y ++ ) {
         for (x = 0; x < xs ; x ++ ) {
-            id = "g" + board[x][y]
+            id = "g" + this.tiles[x][y]
             if (((x+y) % 2) == 0)
                 filling = "yellow"
             else
                 filling = "black"
-            d3.select("svg").append("g").attr("id", id).attr("transform", "translate(" +x*50 + ", " + y * 50 + ")")
-            d3.select("#"+ id).append("rect").attr("width", "50").attr("height", "50").attr("style", "fill : "+filling+"; stroke: black; opacity: 0.3")
-            d3.select("#"+ id).append("text").attr("dominant-baseline", "middle").attr("text-anchor", "middle").attr("x", "25").attr("y", "25").text(formatter.format(board[x][y]))
+            d3.select("svg").append("g").attr("id", id).attr("transform", "translate(" +x*this.cellSize + ", " + y * this.cellSize + ")")
+            d3.select("#"+ id).append("rect").attr("width", this.cellSize).attr("height", this.cellSize).attr("style", "fill : "+filling+"; stroke: black; opacity: 0.3")
+            d3.select("#"+ id).append("text").attr("dominant-baseline", "middle").attr("text-anchor", "middle").attr("x", this.cellSize/2).attr("y", this.cellSize/2).text(formatter.format(this.tiles[x][y]))
         }
     }
 }
 
-drawBoard(createBoard())
+
+theBoard.placeKnight = function() {
+    d3.select("#g1").append("use").attr("xlink:href", "#knight").attr("transform", "scale(0.7)").attr("id", "knight")
+    this.knight = {
+        "pos" : this.center,
+        "visitedFields" : new Set([])
+    }
+}
+
+theBoard.moveKnight = function() {
+    var current = this.knight.pos 
+    var currentVal = this.tiles[current.x][current.y]
+    this.knight.visitedFields.add( currentVal )
+    var possibleMoves = [{"pos" : current.move(+2, +1), "value": this.tiles[current.x+2][current.y+1]}, 
+                         {"pos" : current.move(+2, -1), "value": this.tiles[current.x+2][current.y-1]},
+                         {"pos" : current.move(-2, +1), "value": this.tiles[current.x-2][current.y+1]},
+                         {"pos" : current.move(-2, -1), "value": this.tiles[current.x-2][current.y-1]},
+                         {"pos" : current.move(+1, +2), "value": this.tiles[current.x+1][current.y+2]}, 
+                         {"pos" : current.move(+1, -2), "value": this.tiles[current.x+1][current.y-2]},
+                         {"pos" : current.move(-1, +2), "value": this.tiles[current.x-1][current.y+2]},
+                         {"pos" : current.move(-1, -2), "value": this.tiles[current.x-1][current.y-2]}]
+    var visited = this.knight.visitedFields
+    possibleMoves = possibleMoves.filter(function (x) {
+        return !visited.has(x.value)
+    }).sort(function(a,b){
+        return a.value - b.value
+    })
+    var nextMove = possibleMoves[0].value
+    this.knight.pos = possibleMoves[0].pos
+    // move the knight
+    d3.select("#knight").remove()
+    d3.select("#g" + nextMove).append("use").attr("xlink:href", "#knightDef").attr("transform", "scale(0.7)").attr("id", "knight")
+    d3.select("#g" + currentVal).attr("style", "opacity : 0.1 ")
+}
+
+
+theBoard.draw()
+theBoard.placeKnight()
+
+for (i = 0; i< 1000;i++){
+    setTimeout(function() {theBoard.moveKnight()}, i*50)
+}
